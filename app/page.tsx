157bf281e5403b852ff8ba1ea986ea0c34a08d18
Батчулуун Цогбаяр <batculuun@Batculuuns-MacBook-Pro.local>
2025-12-07 21:36:01 +0800

    add spinner

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import ReactAutoComplete from "./components/react-auto-complete";
 export default function Home() {
   return (
-    <div className="flex ">
+    <div className="flex bg-amber-50">
       <ReactAutoComplete />
     </div>
   );
--- app/spinner/page.tsx
+++ app/spinner/page.tsx
@@ -1,38 +1,6 @@
-import { Spinner } from "@heroui/spinner";
+"use client";
+import { Spinner } from "@heroui/react";
 
 export default function App() {
-  return (
-    <div className="flex flex-wrap items-end gap-8 ">
-      <Spinner
-        classNames={{ label: "text-foreground mt-4" }}
-        label="default"
-        variant="default"
-      />
-      <Spinner
-        classNames={{ label: "text-foreground mt-4" }}
-        label="simple"
-        variant="simple"
-      />
-      <Spinner
-        classNames={{ label: "text-foreground mt-4" }}
-        label="gradient"
-        variant="gradient"
-      />
-      <Spinner
-        classNames={{ label: "text-foreground mt-4" }}
-        label="spinner"
-        variant="spinner"
-      />
-      <Spinner
-        classNames={{ label: "text-foreground mt-4" }}
-        label="wave"
-        variant="wave"
-      />
-      <Spinner
-        classNames={{ label: "text-foreground mt-4" }}
-        label="dots"
-        variant="dots"
-      />
-    </div>
-  );
+  return <Spinner color="warning" label="primary" size="md" />;
 }
